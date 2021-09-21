c09e35eaf6ce03c5e39a72a75ecf35b09b1efd3b
Anthony <delantho327@gmail.com>
2021-09-21 03:59:52 -0500

    changed contact information into table,need to add styling and map

--- src/contact.js
+++ src/contact.js
@@ -1,40 +1,36 @@
+import Table from 'react-bootstrap/Table'
+
 function Contact(){
   return(
-    <table class="contact">
-      <td class ="left">
-      <h1> Locations: </h1>
-      <h3> San Antonio </h3>
-      <p> 3491 Orlando St, San Antonio 78291</p>
-      <h3> Austin </h3>
-      <p> 9012 Cliff Point Ave, Austin 88291</p>
-      <h3> Fort Worth </h3>
-      <p> 27192 Restoration drive, Fort Worth 92010</p>
-      <h3> Corpus Cristi </h3>
-      <p> 19200 Seaside View, Corpus Cristi 72890</p>
-      </td>
-      <td class ="center">
-      <h1>Phone #'s:</h1>
-      <h3>San Antonio:</h3>
-      <p> 210-305-1032</p>
-      <h3> Austin </h3>
-      <p> 391-290-2912</p>
-      <h3> Fort Worth </h3>
-      <p> 409-420-6995</p>
-      <h3> Corpus Cristi </h3>
-      <p> 209-503-5402</p>
-      </td>
-      <td>
-      <h1>Hours:</h1>
-      <h3>9am-10pm</h3>
-      <br />
-      <h3>11am - 11pm</h3>
-      <br />
-      <h3>10am-6pm </h3>
-      <br />
-      <h3>8am-10pm </h3>
-      <br />
-      </td>
-    </table>
+    <div>
+    <Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>Locations</th>
+      <th>Address</th>
+      <th>Phone number</th>
+      <th>Hours</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>San Antonio</td>
+      <td>Austin</td>
+      <td>Corpus Cristi</td>
+    </tr>
+    <tr>
+      <td>210-305-1032</td>
+      <td>210-290-2912</td>
+      <td>409-420-6995</td>
+    </tr>
+    <tr>
+      <td>10am-12am</td>
+      <td colSpan="2">9am - 1am</td>
+      <td>12pm - 10pm</td>
+    </tr>
+  </tbody>
+</Table>
+    </div>
   );
 }
 export default Contact;
